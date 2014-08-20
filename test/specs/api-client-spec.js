2ae5f496402308755c105bb9ce75eb89d76b9bf8
Javier Cervantes <1.27201@gmail.com>
2014-08-20 12:58:41 -0500

    Adding stubbed tests for login promises

--- test/specs/api-client-spec.js
+++ test/specs/api-client-spec.js
@@ -3,20 +3,45 @@ var Spark = source('spark-api');
 describe("Spark", function() {
   describe("login", function() {
 
+    afterEach(function() {
+      Spark.request.restore();
+    });
+
     describe("with wrong credentials", function() {
-      it("returns invalid client message", function() {
-        login_promise = Spark.login();
+
+      beforeEach(function() {
+        sinon.stub(Spark, 'request').yields(null, null, {
+          error: 'invalid_client'
+        });
+      });
+
+      it("returns body error", function() {
+        login_promise = Spark.login('user', 'pass');
         error = 'invalid_client';
 
         return expect(login_promise).to.be.rejectedWith(error);
       });
 
-      it("returns invalid grant message", function() {
-        login_promise = Spark.login('user', 'password');
-        error = 'invalid_grant'
+    });
 
-        return expect(login_promise).to.be.rejectedWith(error);
+    describe("with correct credentials", function() {
+
+      beforeEach(function() {
+        sinon.stub(Spark, 'request').yields(null, null, {
+          access_token: 'access_token'
+        });
+      });
+
+      it("is fulfilled", function() {
+        login_promise = Spark.login('spark', 'spark');
+        return expect(login_promise).to.be.fulfilled;
+      });
+
+      it("returns access token", function() {
+        login_promise = Spark.login('spark', 'spark');
+        return expect(login_promise).to.eventually.equal('access_token');
       });
     });
+
   });
 });
