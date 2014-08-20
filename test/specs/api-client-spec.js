5a11c5ba94cd7cfe3d6826b43e61819a9f009fc1
Javier Cervantes <1.27201@gmail.com>
2014-08-20 11:49:29 -0500

    Correcting handlers

--- test/specs/api-client-spec.js
+++ test/specs/api-client-spec.js
@@ -6,14 +6,14 @@ describe("Spark", function() {
     describe("with wrong credentials", function() {
       it("returns invalid client message", function() {
         login_promise = Spark.login();
-        error = 'Login failed: invalid_client';
+        error = 'invalid_client';
 
         return expect(login_promise).to.be.rejectedWith(error);
       });
 
       it("returns invalid grant message", function() {
         login_promise = Spark.login('user', 'password');
-        error = 'Login failed: invalid_grant'
+        error = 'invalid_grant'
 
         return expect(login_promise).to.be.rejectedWith(error);
       });
