30dc190ce95e04ee47647bead2677592250f4118
edgarsilva <edgar.osc@gmail.com>
2014-08-27 15:18:21 -0500

    WIP on refactor of promises/callbacks/events and defaultHandlers.

---
+++ examples/login-promise.js
@@ -0,0 +1,26 @@
+/*jslint node: true */
+"use strict";
+
+// We start by requiring sparkjs as usual
+// this will give you back an instance of the SparkApi library
+var Sparkjs =require('sparkjs');
+
+// Spark API login
+// You can interact with the api using callbacks, events or promises
+// choose the one you feel more comfortable with, in this example we'll
+// be using Promises
+
+// Storing the promise returned by the the Sparkjs.login() fn.
+// Have in mind that if callback is provided or event listener
+// registered the returned value will be null instead of a Promise.
+var promise = Sparkjs.login({ username: 'email@example.com', password: 'password' });
+
+// Registering functions for promise resolve and reject.
+promise.then(
+  function(token){
+    console.log('API call completed on promise resolve: ', token);
+  },
+  function(err) {
+    console.log('API call completed on promise fail: ', err);
+  }
+);
--- examples/login.js
+++ examples/login.js
@@ -8,24 +8,15 @@ var Sparkjs =require('sparkjs');
 // Spark API login
 // You can interact with the api using callbacks, events or promises
 // choose the one you feel more comfortable with.
+// We are using events and callbacks in this example, usually you would
+// only use one of the two, we are using both here just as an example.
 
 // Registering a listerner for the login event.
 Sparkjs.on('login', function(err, body) {
   console.log('API call completed on Login event:', body);
 });
 
-// Storing the promise returned by the the Sparkjs.login() fn and also passing a callback.
 // The callback will be executed upon completion and returns err and data.
-var promise = Sparkjs.login('email@example.com', 'password', function(err, body) {
+Sparkjs.login({ username: 'email@example.com', password: 'password' }, function(err, body) {
   console.log('API call login completed on callback:', body);
 });
-
-// Registering functions for promise resolve and reject results.
-promise.then(
-  function(token){
-    console.log('API call completed on promise resolve: ', token);
-  },
-  function(err) {
-    console.log('API call completed on promise fail: ', err);
-  }
-);
