bd2ba194fefe59aebbc7987eadb2a8a605ccbe96
David Middlecamp <david@spark.io>
2014-12-08 14:19:25 -0500

    adding basic stream processor to parse incoming Server-Sent-Events, fixing login syntax in example - fixes #32

--- examples/node/get-event-stream.js
+++ examples/node/get-event-stream.js
@@ -1,27 +1,29 @@
 /*jslint node: true */
 "use strict";
 
+//var spark = require('../../lib/spark');
 var spark = require('spark');
 
 spark.on('login', function() {
 
   //Get all events
   spark.getEventStream(false, false, function(data) {
-    console.log("Event: " + data);
+    console.log("Event: " + JSON.stringify(data));
   });
 
   //Get your devices events
   spark.getEventStream(false, 'mine', function(data) {
-    console.log("Event: " + data);
+    console.log("Event: " + JSON.stringify(data));
   });
 
   //Get test event for specific core
   spark.getEventStream('test', 'CORE_ID', function(data) {
-    console.log("Event: " + data);
+    console.log("Event: " + JSON.stringify(data));
   });
 
 
 });
 
 // Login as usual
-spark.login('email@example.com', 'password');
+//spark.login({ username: 'email@example.com', password: 'password'});
+//spark.login({ accessToken: '012345' });
